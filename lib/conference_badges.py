7fb7e5907fbece981588db36ead383b3d526049b
Faith Kariuki <tcwamboi@gmail.com>
2024-08-31 09:47:31 +0300

    Complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = f"Hello, my name is {name}."
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+    room_assignments = []
+    for i, name in enumerate(names):
+        room = rooms[i % len(rooms)]
+        room_assignments.append(f"Hello, {name}! You'll be assigned to room {room}!")
+    
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    room_assignments = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for assignment in room_assignments:
+        print(assignment)
